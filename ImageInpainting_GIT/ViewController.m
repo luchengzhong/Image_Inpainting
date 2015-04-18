71c12aaa688428fcdee0f11f03dbe9af72bc69fb
钟路成 <zhonglucheng@zhongluchengdeMacBook-Pro.local>
2015-04-19 00:05:36 +0800

    add bg

--- ImageInpainting_GIT/ViewController.m
+++ ImageInpainting_GIT/ViewController.m
@@ -1,13 +1,13 @@
 //
 //  ViewController.m
-//  ImageInpainting_GIT
+//  ImageInpainting
 //
-//  Created by 钟路成 on 15/4/18.
+//  Created by 钟路成 on 15/4/6.
 //  Copyright (c) 2015年 钟路成. All rights reserved.
 //
 
 #import "ViewController.h"
-
+#import "MainView.h"
 @interface ViewController ()
 
 @end
@@ -16,6 +16,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].applicationFrame];
+    
+    MainView * myView = [[MainView alloc] initWithFrame:CGRectMake(self.view.bounds.origin.x,self.view.bounds.origin.y+20, self.view.bounds.size.width, self.view.bounds.size.height) ];
+    
+    //UIImage *myImageObj = [UIImage imageNamed:@"Cloth.png"];
+    self.view = myView;
+    //UIImageView * zoomView = [[UIImageView alloc] initWithImage:myImageObj];
+    //[self.view addSubview:zoomView];
+    //[self.view addSubview:myView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
